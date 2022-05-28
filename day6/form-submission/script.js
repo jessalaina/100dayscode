cfd32d9f1e77107cdad1d59870fcfd385a695292
jessalaina <jessalaina90@gmail.com>
2022-05-28 08:48:29 -0400

    basic validation with regex

--- day6/form-submission/script.js
+++ day6/form-submission/script.js
@@ -1,6 +1,15 @@
 const form = document.querySelector('.signup-form');
+const feedback = document.querySelector('.feedback');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    console.log(form.username.value);
+
+    // validate using regex
+    const username = form.username.value;
+    const usernamePattern = /^[a-zA-Z]{6,12}$/
+    if(usernamePattern.test(username)){
+        feedback.textContent = 'Username valid. Submitted';
+    } else {
+        feedback.textContent = 'Username must contain letters only and be between 6 & 12 characters long.'
+    }
 })
