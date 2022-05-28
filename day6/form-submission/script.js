7ee1e2a6c688a4fc441c0400ca4ad63f1e515fa4
jessalaina <jessalaina90@gmail.com>
2022-05-28 09:01:15 -0400

    live form validation

--- day6/form-submission/script.js
+++ day6/form-submission/script.js
@@ -1,15 +1,25 @@
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     // validate using regex
     const username = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,12}$/
+
     if(usernamePattern.test(username)){
         feedback.textContent = 'Username valid. Submitted';
     } else {
         feedback.textContent = 'Username must contain letters only and be between 6 & 12 characters long.'
+    }
+})
+
+// live feedback
+form.username.addEventListener('keyup', e => {
+    if(usernamePattern.test(e.target.value)){
+        form.username.setAttribute('class', 'success');
+    } else {
+        form.username.setAttribute('class', 'error');
     }
 })
