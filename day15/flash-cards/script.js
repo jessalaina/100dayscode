3d7e9b73add9194ec15700eea3b1ca37a69ed464
jessalaina <jessalaina90@gmail.com>
2022-06-06 14:44:15 -0400

    create function to flip cards

--- day15/flash-cards/script.js
+++ day15/flash-cards/script.js
@@ -1,13 +1,26 @@
+/*
+** have area where user can input what they want to memorize and the answer
+** on click new flashcard will be added to screen
+** on screen will see front of card
+** on click of arrow button, flip card on y-axis to see answer
+*/
+
 const save = document.querySelector('.save');
 const container = document.querySelector('.container');
 const newTitle = document.querySelector('.new-title');
 const newAnswer = document.querySelector('.new-answer');
+const flipBtns = document.querySelectorAll('.bi');
 
+// click to add flashcard
 save.addEventListener('click', addCard);
 
+// add div html to add inputs to screen
 function addCard() {
+    // created the needed div
     const newDiv = document.createElement('div');
+    // add class card to match for bootstrap styles
     newDiv.classList.add('card');
+    // add all internal html to get card on screen
     newDiv.innerHTML = `<div class="card">
     <div class="card-body">
         <h3 class="card-title">
@@ -19,5 +32,18 @@ function addCard() {
         <p class="card-text">${newAnswer.value}</p>
     </div>
 </div>`;
+    // append new div to container
     container.append(newDiv);
+    newTitle.value = '';
+    newAnswer.value = '';
+}
+
+// loop through flip buttons
+flipBtns.forEach(btn => {
+    btn.addEventListener('click', flipCard);
+})
+
+// add flip card abilities
+function flipCard() {
+    console.log('hi');
 }
