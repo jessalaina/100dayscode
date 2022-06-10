d7ab76c8743abc65545176905cd22bd7e10dd088
jessalaina <jessalaina90@gmail.com>
2022-06-10 16:54:37 -0400

    flip all cards one at a time when clicked

--- day19/matching-game/script.js
+++ day19/matching-game/script.js
@@ -1,24 +1,14 @@
 /*
-** click on cards one at a time (randomly) to try to match 2
-** loop through card divs -- get single event click
-** when 1 card is clicked it will flip and stay flipped for 3 seconds (setInterval) unless matched to another card
-** on match, remove cards from "cards" container
-** continue until all cards gone
+** onclick, flip card over
 */
 
-const cards = document.querySelectorAll('.back');
-const front = document.querySelector('.front');
+const cards = document.querySelectorAll('.card');
 
-// loop through cards adding click event listener
+
 cards.forEach(card => {
-    card.addEventListener('click', (e) => {
-        // get current display of card
-        let display = front.style.display;
-
-        if (display === 'block') {
-            front.style.display = 'none';
-        } else {
-            front.style.display = 'block';
-        }
-    });
+    card.addEventListener('click', flipCard);
 })
+
+function flipCard(e) {
+    e.target.classList.toggle('flip-card');
+}
