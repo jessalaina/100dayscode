3578286c87c3ef837ffc0c980669e91e21f7e47b
jessalaina <jessalaina90@gmail.com>
2022-06-22 20:15:44 -0400

    day 31 understand how to add string to screen from array of objects

--- day29-day30-day31/cause-effect/script.js
+++ day29-day30-day31/cause-effect/script.js
@@ -3,10 +3,10 @@ import {people} from './data.js';
 const mainContent = document.querySelector('main');
 const para = document.createElement('p');
 
-let names = Object.values(people);
+
 
 for (let person of people) {
-    console.log(person);
-    para.innerHTML = person.name;
+    para.innerHTML = JSON.stringify(person.name);
     mainContent.appendChild(para);
 }
+
