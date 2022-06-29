f8549e3c3516fa1f11e6bd46c506e2ee593b99c4
jessalaina <jessalaina90@gmail.com>
2022-06-29 19:26:18 -0400

    day38 bubble pop continued, reset button resets game

--- day38/bubble-pop/script.js
+++ day38/bubble-pop/script.js
@@ -1,18 +1,13 @@
 const bubbles = document.getElementById('bubbles');
 const bubble = document.querySelectorAll('.bubble');
-const reset = document.querySelector('.reset');
 
 for(const bub of bubble) {
     bub.addEventListener('mouseover', (e) => {
         console.log(e.target);
         if (e.target === bub) {
             bub.classList.add('hidden');
+            // bubbles.classList.add('padding');
+            // bubbles.textContent = 'POP!!';
         }
     })
 }
-
-reset.addEventListener('click', () => {
-    for(const bub of bubble) {
-        bub.classList.remove('hidden');
-    }
-})
